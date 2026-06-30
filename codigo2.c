8d3ae3527f9a5d1e613759752900238469b23517
Rudah S Jesus <156484678+rudahjesus@users.noreply.github.com>
2026-06-30 12:10:43 -0300

    Create codigo2.c

---
+++ codigo2.c
@@ -0,0 +1,121 @@
+//integrantes: Rudah Santos de Jesus, Isaac José Guedes Pinheiro de Matos, Raquel de Santana Nascimento, Alvaro Matheus Santos de Oliveira, João Vitor Rodrigues Coelho Ferreira
+// Esse é um código que simula o movimento de um robô seguidor de linha utilizando um sistema de controle proporcional
+// O robô tem 2 rodas (motores) e 4 sensores. O Código gera um valor aleatório entre 0 e 255 para os sensores como posição inicial
+// O 0 significa que o robô não está lendo a linha e 255 significa que o robô está lendo perfeitamente a linha
+// Porém, intervalos entre 0 e 70 podem significar que o sensor está fora da linha, e entre 80 e 255 geralmente significam acima da linha
+// O código se propõe a forçar os dois sensores do meio (2 e 3) a ficarem em cima da linha, enquanto os dos cantos (1 e 4) ficam fora da linha
+// O código foi feito e testado em online gbd, então, se utilizando gcc, vscode ou devc++, pode ser necessário modificar algumas bibliotecas
+// Como por exemplo, <unistd.h> pode precisar ser trocado por <windows.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main()
+{
+    int speedMotorA = 10;
+    int speedMotorB = 10;
+
+    int sensor0, sensor1, sensor2, sensor3;
+    int sensor[4];
+
+    srand(time(NULL));
+
+    sensor0 = rand();
+    sensor1 = rand();
+    sensor2 = rand();
+    sensor3 = rand();
+
+    sensor[0] = sensor0;
+    sensor[1] = sensor1;
+    sensor[2] = sensor2;
+    sensor[3] = sensor3;
+
+    for(int k = 0; k < 4; k++)
+    {
+        sensor[k] = sensor[k] / 100;
+
+        if(sensor[k] <= 0)
+        {
+            sensor[k] = 0;
+        }
+
+        if(sensor[k] >= 256)
+        {
+            sensor[k] = 256;
+        }
+    }
+
+    for(int i = 0; i < 20; i++)
+    {
+        int nextSpeedMotorA = 0;
+        int nextSpeedMotorB = 0;
+
+        if(sensor[0] >= 100 && sensor[1] >= 100)
+        {
+            sensor[2] = sensor[2] / 10;
+            sensor[3] = sensor[3] / 10;
+        }
+
+        if(sensor[2] >= 100 && sensor[3] >= 100)
+        {
+            sensor[0] = sensor[2] / 10;
+            sensor[1] = sensor[3] / 10;
+        }
+
+        if(sensor[1] >= 100 && sensor[2] >= 100)
+        {
+            sensor[0] = sensor[2] / 10;
+            sensor[3] = sensor[3] / 10;
+        }
+
+        sensor[0] = sensor[0] + speedMotorB;
+        sensor[1] = sensor[1] + speedMotorB * 2;
+        sensor[2] = sensor[2] + speedMotorA * 2;
+        sensor[3] = sensor[3] + speedMotorA;
+
+        for(int k = 0; k < 4; k++)
+        {
+            if(sensor[k] <= 0)
+            {
+                sensor[k] = 0;
+            }
+
+            if(sensor[k] >= 256)
+            {
+                sensor[k] = 256;
+            }
+        }
+
+        nextSpeedMotorA =
+            sensor[0] / 2 +
+            sensor[1] / 4 -
+            sensor[2] / 4 -
+            sensor[3] / 2;
+
+        nextSpeedMotorB =
+            sensor[3] / 2 +
+            sensor[2] / 4 -
+            sensor[1] / 4 -
+            sensor[0] / 2;
+
+        printf("sensor 0: %d || ", sensor[0]);
+        printf("sensor 1: %d || ", sensor[1]);
+        printf("sensor 2: %d || ", sensor[2]);
+        printf("sensor 3: %d\n", sensor[3]);
+
+        printf("prox. vel. motor A = %d || prox. vel. motor B = %d\n",
+               nextSpeedMotorA,
+               nextSpeedMotorB);
+
+        printf("---------------------------------------------------------------------\n");
+
+        speedMotorA = nextSpeedMotorA;
+        speedMotorB = nextSpeedMotorB;
+
+        sleep(2);
+    }
+
+    return 0;
+}
